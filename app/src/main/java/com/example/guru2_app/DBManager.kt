a684d6345e9fb663510de61ce11ba8e21f0ae4dd
Resfeber <69678585+Resfebel@users.noreply.github.com>
2023-07-26 21:43:46 +0900

    Update DBManager.kt
    Add dress DB

---
+++ app/src/main/java/com/example/guru2_app/DBManager.kt
@@ -0,0 +1,34 @@
+package com.example.guru2_app
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DBManager(
+    context: Context?,
+    name: String?,
+    factory: SQLiteDatabase.CursorFactory?,
+    version: Int
+) : SQLiteOpenHelper(context, name, factory, version) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        // 유저 정보 db
+        db!!.execSQL("CREATE TABLE user (" +
+                "email text PRIMARY KEY NOT NULL," +
+                "id text NOT NULL, pw text NOT NULL, " +
+                "userName text DEFAULT null, ikkiName text DEFAULT null," +
+                "ikkiLevel INTEGER DEFAULT 0, coin INTEGER DEFAULT 0, todoCount INTEGER DEFAULT 0);")
+
+        // 투두리스트 정보 db
+        db!!.execSQL("CREATE TABLE todo (email_fk text, date date, listIndex INTEGER, list text," +
+                "FOREIGN KEY (email_fk) REFERENCES user (email) );")
+
+        // 옷 정보 db
+        db!!.execSQL("CREATE TABLE dress (email_fk2 text, item1 INTEGER, " +
+                "item2 INTEGER, item3 INTEGER, item4 INTEGER, item5 INTEGER, " +
+                "FOREIGN KEY (email_fk2) REFERENCES user (email));")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
+
+    }
+}
